965354b1596f4292b9a6bcbef6b41e1624e6af3e
xxzeeeeeexx <zayannazeenath@gmail.com>
2025-07-02 22:20:09 +0400

    Scribble -> MERN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,7 @@ import toast from "react-hot-toast"
 
 const App = () => {
   return (
-    <div className='relative h-full w-full'> 
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#ff510040_100%)]" />
+    <div className='relative h-full w-full'>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ return <header className="bg-base-300 border-b border-base-content/10">
 
     <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
-                <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">Scribble</h1>
+                <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">MERN</h1>
                 <div className="flex items-center gap-4">
                         <Link to={"/create"} className="btn btn-primary">
                         <PlusIcon className="size-5"/>
